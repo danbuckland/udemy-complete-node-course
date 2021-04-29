371911cb86e8c42c82feb8ee46878a825864d860
Dan Buckland <danbucklan@gmail.com>
2021-04-29 14:19:46 +0100

    Complete section 8

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,27 @@
-console.log('Client side JS loaded!')
+// Client-side JavaScript which is different from Node
+const weatherForm = document.querySelector('form')
+const searchEl = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+const fetchWeather = (location) => {
+  messageOne.textContent = 'fetching results...'
+  messageTwo.textContent = ''
+  fetch(`/weather?address=${location}`).then((response) => {
+  response.json().then((data) => {
+    if (data.error) {
+      messageOne.textContent = data.error
+      messageTwo.textContent = ''
+    } else {
+      messageOne.textContent = data.location
+      messageTwo.textContent = data.forecast
+    }
+    })
+  })
+}
+
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  fetchWeather(searchEl.value)
+})
