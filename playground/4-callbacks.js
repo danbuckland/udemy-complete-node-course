fd42f09dfaabfd02e184a19091d80bc4543aa4f6
Dan Buckland <danbucklan@gmail.com>
2021-10-08 14:18:45 +0100

    Add promise and callback examples

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -1,35 +1,14 @@
-setTimeout(() => {
-  console.log('2 seconds are up')
-}, 2000)
-
-const names = ['Dan', 'Narges', 'Mike', 'Harvey']
-
-const shortNames = names.filter((name) => name.length <= 4)
-
-console.log(shortNames)
-
-const geocode = (address, callback) => {
+const doWorkCallback = (callback) => {
   setTimeout(() => {
-    const data = {
-      latitude: 0,
-      longitude: 0,
-    }
+    // callback(undefined, [1, 4, 7])
+    callback('Something went wrong!')
+  }, 2000)
+}
+
+doWorkCallback((error, result) => {
+  if (error) {
+    return console.log(error)
+  }
   
-    callback(data)
-  }, 2000)
-}
-
-geocode('London', (data) => {
-  console.log(data)
-})
-
-const add = (a, b, callback) => {
-  setTimeout(() => {
-    const sum = a + b
-    callback(sum)
-  }, 2000)
-}
-
-add(1, 4, (sum) => {
-  console.log(sum) // Should print: 5
+  console.log(result)
 })
